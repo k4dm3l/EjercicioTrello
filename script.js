5539f054a87110328e2bf8a2c8b4af4b768aa6e5
Camilo <c.acevedo1993@gmail.com>
2019-08-29 22:37:08 -0500

    Add function to delete task from a list

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ let app = new Vue({
                 status: 'creating',
                 tasks: [ ]
             };
+
             this.lists.push(newList);
         },
         saveList(list){
-            list.status = 'saved';
+            if(this.verifyEmptyList(list)){
+                alert('The list can`t be empty');
+            } else {
+                list.status = 'saved';
+            }
         },
         removeList(index){
             this.lists.splice(index, 1);
@@ -23,8 +28,33 @@ let app = new Vue({
             const newTask = {
                 info: info
             };
-            this.lists[index].tasks.push(newTask);
-            document.getElementById('task'+index).value = '';
+            
+            if(this.verifyEmptyTask(newTask)){
+                alert('The task can`t be empty!');
+            } else {
+                this.lists[index].tasks.push(newTask);
+                document.getElementById('task'+index).value = '';
+            }
+        },
+        deleteTask(tasks, index){
+            tasks.splice(index, 1);
+        },
+        updateTask(){
+            alert('Here UPDATE task');
+        },
+        verifyEmptyTask(task){
+            if(task.info == ''){
+                return true;
+            } else {
+                return false;
+            }
+        },
+        verifyEmptyList(list){
+            if(list.name == ''){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 });
